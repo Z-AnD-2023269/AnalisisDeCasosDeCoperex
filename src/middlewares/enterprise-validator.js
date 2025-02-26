e8cce1afdbd15029f95963ecf10b8c841ca623d6
Z-AnD-2023269 <alopez-2023269@kinal.edu.gt>
2025-02-26 15:55:37 -0600

    feat: added enterprise listing with filters, update functionality, routes, and validators

--- src/middlewares/enterprise-validator.js
+++ src/middlewares/enterprise-validator.js
@@ -1,6 +1,6 @@
 `use strict`;
 
-import { body } from "express-validator";
+import { body, query } from "express-validator";
 import { enterpriseExists } from "../helpers/db-validators.js";
 import { validarCampos } from "./validate-fields.js";
 import { handleErrors } from "./handle-errors.js";
@@ -22,3 +22,23 @@ export const validateEnterprise = [
     validarCampos,
     handleErrors
 ]
+
+export const validateGetEnterprises = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
+    query('category').optional().isString().withMessage("La categoría debe ser una cadena de texto."),
+    query('impactLevel').optional().isIn(['Alto', 'Medio', 'Bajo']).withMessage("El nivel de impacto debe ser uno de los siguientes: Alto, Medio, Bajo."),
+    query('yearsOfExperience').optional().isInt({ min: 0 }).withMessage("Los años de experiencia deben ser un número entero positivo."),
+    query('sort').optional().isIn(['nameAZ', 'nameZA', 'experience']).withMessage("El valor de ordenamiento debe ser uno de los siguientes: nameAZ, nameZA, experience."),
+    query('limite').optional().isInt({ min: 1, max: 100 }).withMessage("El límite debe ser un número entero entre 1 y 100."),
+    query('desde').optional().isInt({ min: 0 }).withMessage("El índice desde debe ser un número entero no negativo."),
+    validarCampos,
+    handleErrors
+]
+
+export const updateEnterpriseValidator = [
+    validateJWT,
+    validarCampos,
+    handleErrors
+]
+
